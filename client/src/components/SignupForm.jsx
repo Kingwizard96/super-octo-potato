8428750dd2ddca5fadc074b15badddbd1fcc7b51
Ra <ranilsd86@gmail.com>
2024-01-10 13:54:24 -0500

    got main page to render

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -39,7 +39,7 @@ const SignupForm = () => {
 
     try {
       const { data } = await addUser({
-        variables: { ...userFormData }
+        variables: { ...userFormData },
       });
       Auth.login(data.addUser.token);
       // console.log(data);
